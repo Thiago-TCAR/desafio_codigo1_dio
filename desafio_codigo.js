f5b8224b6397faa9294475a460c2ca6603b7e530
Thiago Ramos <thiago_c_a_ramos@hotmail.com>
2024-10-29 19:08:47 -0300

    Desafio DIO

--- desafio_codigo.js
+++ desafio_codigo.js
@@ -1,1 +1,23 @@
-console.log("Thiago DEV")
+let name = "Thiago"
+let nivelXP = 8000
+let classificacao = ""
+
+if (nivelXP >= 0 && nivelXP <= 1001) {
+    classificacao = "Ferro";
+} else if (nivelXP <= 2000) {
+    classificacao = "Bronze";
+} else if (nivelXP <= 5000) {
+    classificacao = "Prata";
+} else if (nivelXP <= 7000) {
+    classificacao = "Ouro";
+} else if (nivelXP <= 8000) {
+    classificacao = "Platina";
+} else if (nivelXP <= 9000) {
+    classificacao = "Ascendente";
+} else if (nivelXP <= 10000) {
+    classificacao = "Imortal";
+} else {
+    classificacao = "Radiante";
+}
+
+console.log("O herói de nome " + name + " está no nível " + classificacao)
